785ccebc416dd43fff7810c4284d21dc4d26d623
Gedoni Ani <gedoniani@gmail.com>
2023-11-07 11:34:19 +0100

    new files added

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Index from "../views/Index.vue";
+
+const routes = [
+
+    {
+        path: "/",
+        name: 'Index',        
+        component: Index
+    },
+
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
